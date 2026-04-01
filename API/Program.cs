11c9f493c437ba4229d62f0f3798b0006be804cb
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-04-01 13:21:17 +0300

    feat: configure CORS policy to allow requests from specified origins

--- API/Program.cs
+++ API/Program.cs
@@ -3,9 +3,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
+app.UseCors(opt =>
+    opt.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
+    
 app.MapControllers();
 
 app.Run();
