9f2c7a2407a80e9aa8eb5b60c0ae3c00b8c00b3e
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-04-11 13:12:16 +0300

    feat: implement authentication structure with DTOs, controllers, and service extensions

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,11 @@
+using API.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DbContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 
