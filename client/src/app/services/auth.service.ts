3e1b9d0d92a827a1f5bd5e181f293fb711a0c197
Suhan Mihai Andrei <suhanandrei2004@gmail.com>
2026-04-22 10:36:17 +0300

    feat: implement AuthService for user authentication with login, registration, and logout methods

---
+++ client/src/app/services/auth.service.ts
@@ -0,0 +1,42 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { User } from '../../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  baseUrl = environment.apiUrl;
+  currentUser = signal<User | null>(null);
+
+  login(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      })
+    )
+  }
+
+  register(model: any) {
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      })
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
